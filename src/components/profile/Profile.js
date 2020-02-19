9319fed93a4abbba6ddf944065e76639f2d2e360
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-02-20 02:57:48 +0700

    [Update] edit size

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,36 +1,48 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
 const Div = styled.div`
-    display: grid;
-    position: relative;
-    justify-items: center;
-    margin: auto 0;
-    height: 90vh;
-    width: 100vw;
-    background-color: #59626282;
-`
+  position: relative;
+  display: flex;
+  flex-flow: wrap;
+  flex-direction: column;
+  margin: auto 0;
+  height: 90vh;
+  width: 100vw;
+  background-color: #59626282;
+`;
 const Img = styled.img`
   border-radius: 50%;
-  width: 20rem;
+  width: 10rem;
   position: relative;
-  margin-top: 3em;
+margin: 3em auto;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-flow: wrap;
+  flex-direction: column;
 `;
 
 const P = styled.p`
-    margin: 0 auto;
-`
+  margin: 0 auto 1em auto;
+  font-size: 25px;
+`;
 
 export default class Profile extends Component {
-    render() {
-        return (
-            <Div>
-                <Img src='https://avatars3.githubusercontent.com/u/31593971?s=400&u=ed8a044aa68008e06ad83be76e67c7a2d789937d&v=4' />
-                <P>Apiwat Aeeabundit</P>
-                <P>Student <br/>
-                @IT, SIT, KMUTT</P>
-                <P>Web Developer</P>
-            </Div>
-        )
-    }
+  render() {
+    return (
+      <Div>
+        <Img src='https://avatars3.githubusercontent.com/u/31593971?s=400&u=ed8a044aa68008e06ad83be76e67c7a2d789937d&v=4' />
+        <Content>
+          <P>Apiwat Aeeabundit</P>
+          <P>
+            Student <br />
+            @IT, SIT, KMUTT
+          </P>
+          <P>Web Developer</P>
+        </Content>
+      </Div>
+    );
+  }
 }
