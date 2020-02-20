941e9f7cd3f1187de2a2f77e88b12fffdfd70ddd
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-02-20 16:12:44 +0700

    [Update] add firebase

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React from 'react';
 import './App.css';
 import Profile from './components/index';
 import { ParallaxProvider } from 'react-scroll-parallax';
+import firebase from 'firebase'
 
 function App() {
+
+  let config = {
+    apiKey: "AIzaSyAoTmAqEkZW13qpv1bQoiILT8vYtj8Fjvg",
+    authDomain: "apiwat-portfolio.firebaseapp.com",
+    databaseURL: "https://apiwat-portfolio.firebaseio.com",
+    projectId: "apiwat-portfolio",
+    storageBucket: "apiwat-portfolio.appspot.com",
+    messagingSenderId: "245133150101",
+    appId: "1:245133150101:web:f0854b82d91c324023589c",
+    measurementId: "G-ZM09M43RPC"
+  };
+  firebase.initializeApp(config);
+
   return (
     <div className='App'>
       <ParallaxProvider>
-        <Profile />
+        <Profile db={firebase}/>
       </ParallaxProvider>
     </div>
   );
