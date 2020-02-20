6b744d2e862ea343a053b3eaa1100d665b7bd06f
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-02-20 16:10:28 +0700

    [Update] prototype complete

--- src/components/feedback/Feedback.js
+++ src/components/feedback/Feedback.js
@@ -1,22 +1,78 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import trim from 'trim';
 
 const Div = styled.div`
-    display: grid;
-    position: relative;
-    justify-items: center;
-    margin: auto 0;
-    height: 90vh;
-    width: 100vw;
-    background-color: #59626282;
+  display: grid;
+  position: relative;
+  justify-items: center;
+  margin: auto 0;
+  height: 90vh;
+  width: 100vw;
+  background-color: #59626282;
+`;
+
+const Form = styled.form`
+    justify-content: center;
+    margin: auto;
+    width: 50vw;
 `
 
+
 export default class Feedback extends Component {
-    render() {
-        return (
-            <Div>
-                <p>FeedBack</p>
-            </Div>
-        )
+  state = {
+    feedback: ''
+  };
+
+  onChange(e) {
+    console.info(e);
+    this.setState({
+      feedback: e.target.value
+    });
+  }
+
+  onKeyup(e) {
+    if (e.keyCode === 13 && trim(e.target.value) !== '') {
+      //   e.preventDefault();
+      let dbCon = this.props.db.database().ref('/feedback');
+      dbCon.push({
+        feedback: trim(e.target.value)
+      });
+      this.setState({
+        feedback: ''
+      });
     }
+  }
+
+  test() {
+    let dbCon = this.props.db.database().ref('/feedback');
+    dbCon.push({
+      feedback: trim(this.state.feedback)
+    });
+    this.setState({
+      feedback: ''
+    });
+  }
+
+  render() {
+    return (
+      <Div>
+        <Form>
+          <textarea
+            className='textarea'
+            placeholder='Type a message'
+            style={{
+                borderRound: "25%",
+                margin: 'auto'
+            }}
+            onChange={this.onChange.bind(this)}
+            onKeyUp={this.onKeyup.bind(this)}
+            value={this.state.feedback}
+          ></textarea>
+          <hr/>
+          <p>Press enter to submit.</p>
+        </Form>
+      </Div>
+    );
+  }
 }
